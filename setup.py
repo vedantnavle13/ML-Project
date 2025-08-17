04ae3a538742bca06c4352fdef8c00b3b4511e00
Vedant Navle <vedantnavale02@gmail.com>
2025-08-17 18:02:13 +0530

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPENATION_PREFIX = '-e .'
+HYPHEN_E_DOT = '-e .'
 
 def get_requirements(file_path:str) -> List[str]:
     """
@@ -12,8 +12,8 @@ def get_requirements(file_path:str) -> List[str]:
     with open(file_path) as file:
         requirements = file.readlines()
         requirements=[req.replace('\n', '') for req in requirements]
-        if HYPENATION_PREFIX in requirements:
-            requirements.remove(HYPENATION_PREFIX)
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
     return requirements
 
     
